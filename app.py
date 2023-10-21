197a3c8573ee07ad372bcb1cc04b7c7a0b11a8ab
MuratAyar <108651456+MuratAyar@users.noreply.github.com>
2023-10-21 20:20:42 +0000

    Fully Functional without My Projects

--- app.py
+++ app.py
@@ -4,33 +4,40 @@ app = Flask(__name__, template_folder='template')
 
 JOBS = [
     {
-        'id': 1,
-        'title': 'Data Analyst',
-        'location': 'İstanbul',
-        'salary': '10,000 TL'
+      'id': 1,
+      'title': 'Volunteer BTK Internship',
+      'location': 'Ankara, Turkey',
+      'position': 'Software Developer',
+      'description': 'We automatically detect the URLs in the notifications received by the USOM department, pass them through malware control, collect the necessary evidence through various URL Scan APIs and forward them to higher authorities for the removal of the site. The project is where we collect the essential evidence for removing a site.',
     },
     {
-        'id': 2,
-        'title': 'Full Stack',
-        'location': 'Ankara',
-        'salary': '20,000 TL'
+      'id': 2,
+      'title': 'Compulsory Turkcell Internship',
+      'location': 'Remote',
+      'position': 'Software Developer',
+      'description': 'I worked in a software developer team which is dedicated to update and upgrade the old version of customer service application of Turkcell on a large scale.My job was coding the unit testings for both old and new version of our applications services and checking if there is any bugs and if there is any, we could easily locate and patch them.',
     },
     {
-        'id': 3,
-        'title': 'Goal Keeper',
-        'location': 'İzmir',
-        'salary': '30,000 TL'
+      'id': 3,
+      'title': 'Long Term Turkcell Internship',
+      'location': 'Remote',
+      'position': 'Software Developer',
+      'description': 'I am currently working in this position...'
     },
 ]
 
 
 @app.route('/')
 def hello_world():
-  return render_template('home.html', jobs=JOBS, company_name='Muratia')
+  return render_template('home.html', jobs=JOBS)
 
 @app.route('/api/jobs')
 def list_jobs():
   return jsonify(JOBS)
+
+@app.route('/contact')
+def contact():
+  return render_template('contact.html')
 
 
 if __name__ == '__main__':
